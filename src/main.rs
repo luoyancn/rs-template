7e746052a2d4ccbb95a1b653d5771309cc0154e1
luoyancn <luoyan_cn@outlook.com>
2021-03-04 14:31:05 +0800

    Update the template projects

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ extern crate log;
 
 extern crate slog_logger;
 
+use slog_stdlog::crit;
+
 fn main() {
-    let logger = slog_logger::initlogger(false, "", 0);
+    let logger = slog_logger::initlogger(false, "", 0, false, false);
     let _guard = slog_scope::set_global_logger(logger);
     slog_stdlog::init().unwrap();
     info!("Hello template");
+    crit!("hello crit");
 }
